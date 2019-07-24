5339e6ba67797a76dc41622289dbe001b10e0579
Alexander Chernov <alekcander@gmail.com>
2019-07-24 19:57:01 +0100

    First Commit

---
+++ db.go
@@ -0,0 +1,98 @@
+package db
+
+import (
+	"fmt"
+	"github.com/davecgh/go-spew/spew"
+	"sync"
+	"time"
+
+	//Mysql driver for gorm
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
+
+var (
+	// Username is a username used for connection
+	Username string
+
+	// Password is a password used for connection
+	Password string
+
+	// Database is a db name to which you want to connect
+	Database string
+
+	// Host for connection
+	Host = "localhost"
+
+	// Port used for connection
+	Port = 3306
+
+	//Charset for the connection.
+	Charset = "utf8"
+
+	// ParseTime defines if we need to parse time columns.
+	ParseTime = "True"
+
+	//Location used for timestamps related functions.
+	Location = "Europe%2FLondon"
+
+	//DebugLog for now unused
+	DebugLog bool
+
+	//Maximum open connections which app can have
+	MaxOpenConnections = 10
+
+	//
+	MaxIdleConnections = 5
+
+	//
+	ConnMaxLifeTime = 10
+)
+
+var instance *gorm.DB
+var dbOnce sync.Once
+
+//Returns (or creates) the singleton instance
+func Instance() *gorm.DB {
+	dbOnce.Do(func() {
+		var err error
+		instance, err = CreateInstance(Username, Password, Database, Host, Port, DebugLog)
+		if err != nil {
+			fmt.Printf("Error on database instance creation. [%s]\n", err)
+			if DebugLog {
+				fmt.Printf("Username [%s], Password [%s], Db [%s], Host [%s:%d]\n", Username, Password, Database, Host, Port)
+				spew.Dump(err)
+			}
+			os.Exit(1)
+		}
+		instance.DB().SetMaxOpenConns(MaxOpenConnections)
+		instance.DB().SetMaxIdleConns(MaxIdleConnections)
+		instance.DB().SetConnMaxLifetime(time.Duration(ConnMaxLifeTime) * time.Second)
+	})
+	return instance
+}
+
+// CreateInstance
+func CreateInstance(username, password, dbName, host string, port int, debugLog bool) (*gorm.DB, error) {
+	var connectionString = fmt.Sprintf(
+		"%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=%s&loc=%s",
+		username,
+		password,
+		host,
+		port,
+		dbName,
+		Charset,
+		ParseTime,
+		Location,
+	)
+
+	dbManager, err := gorm.Open("mysql", connectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	dbManager.LogMode(debugLog)
+	return dbManager, nil
+}
---
+++ go.mod
@@ -0,0 +1,17 @@
+module stash.binweevils.com/go/db
+
+require (
+	cloud.google.com/go v0.31.0 // indirect
+	github.com/davecgh/go-spew v1.1.1
+	github.com/denisenkom/go-mssqldb v0.0.0-20181014144952-4e0d7dc8888f // indirect
+	github.com/erikstmartin/go-testdb v0.0.0-20160219214506-8d10e4a1bae5 // indirect
+	github.com/go-sql-driver/mysql v1.4.0
+	github.com/google/go-cmp v0.2.0 // indirect
+	github.com/jinzhu/gorm v1.9.1
+	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
+	github.com/jinzhu/now v0.0.0-20180511015916-ed742868f2ae // indirect
+	github.com/lib/pq v1.0.0 // indirect
+	github.com/mattn/go-sqlite3 v1.10.0 // indirect
+	golang.org/x/crypto v0.0.0-20181030102418-4d3f4d9ffa16 // indirect
+	google.golang.org/appengine v1.2.0 // indirect
+)
